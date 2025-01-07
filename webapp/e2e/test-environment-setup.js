774134c5ac1a48f083713533af7032edf27435d2
pglez82 <pglez82@gmail.com>
2025-01-07 14:13:25 +0100

    fixing test e2e

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -3,6 +3,7 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 let mongoserver;
 let userservice;
 let authservice;
+let llmservice;
 let gatewayservice;
 
 async function startServer() {
@@ -12,6 +13,7 @@ async function startServer() {
     process.env.MONGODB_URI = mongoUri;
     userservice = await require("../../users/userservice/user-service");
     authservice = await require("../../users/authservice/auth-service");
+    llmservice = await require("../../llmservice/llm-service");
     gatewayservice = await require("../../gatewayservice/gateway-service");
 }
 
