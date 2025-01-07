28d84c2fbebb586b0a57941f01e012192cb6205c
pglez82 <pglez82@gmail.com>
2025-01-07 13:05:22 +0100

    generate llm message

--- webapp/src/components/Login.js
+++ webapp/src/components/Login.js
@@ -2,21 +2,34 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Container, Typography, TextField, Button, Snackbar } from '@mui/material';
+import { Typewriter } from "react-simple-typewriter";
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
   const [error, setError] = useState('');
   const [loginSuccess, setLoginSuccess] = useState(false);
   const [createdAt, setCreatedAt] = useState('');
   const [openSnackbar, setOpenSnackbar] = useState(false);
 
   const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+  const apiKey = process.env.REACT_APP_LLM_API_KEY || 'None';
 
   const loginUser = async () => {
     try {
       const response = await axios.post(`${apiEndpoint}/login`, { username, password });
 
+      const question = "Please, generate a greeting message for a student called " + username + " that is a student of the Software Architecture course in the University of Oviedo. Be nice and polite.";
+      const model = "gemini"
+
+      if (apiKey==='None'){
+        setMessage("LLM API key is not set. Cannot contact the LLM.");
+      }
+      else{
+        const message = await axios.post(`${apiEndpoint}/askllm`, { question, model, apiKey })
+        setMessage(message.data.answer);
+      }
       // Extract data from the response
       const { createdAt: userCreatedAt } = response.data;
 
@@ -37,9 +50,12 @@ const Login = () => {
     <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
       {loginSuccess ? (
         <div>
-          <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
-            Hello {username}!
-          </Typography>
+          <Typewriter
+            words={[message]} // Pass your message as an array of strings
+            cursor
+            cursorStyle="|"
+            typeSpeed={50} // Typing speed in ms
+          />
           <Typography component="p" variant="body1" sx={{ textAlign: 'center', marginTop: 2 }}>
             Your account was created on {new Date(createdAt).toLocaleDateString()}.
           </Typography>
